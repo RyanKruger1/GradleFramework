98fa5ccb940fca3c5cdbb516825afe6b987b10eb
ryank <ryank@globalkinetic.com>
2020-12-02 11:55:28 +0200

    Commit initial files

---
+++ src/test/java/ApiCalls/TestOne.java
@@ -0,0 +1,30 @@
+package ApiCalls;
+
+import org.testng.annotations.Test;
+
+public class TestOne {
+
+    @Test(groups = "smoke")
+    public static void makeCall1()
+    {
+        System.out.println("smoke");
+    }
+
+    @Test(groups = "reg")
+    public static void makeCall2()
+    {
+        System.out.println("reg");
+    }
+
+    @Test(groups = "reg")
+    public static void makeCall3()
+    {
+        System.out.println("reg");
+    }
+
+    @Test(groups = "reg")
+    public static void makeCall4()
+    {
+        System.out.println("reg");
+    }
+}
